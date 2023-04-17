683ce2fa54c6caa4ce5548399d3d58af1665cf26
OneCleverFox <124699699+OneCleverFox@users.noreply.github.com>
2023-04-17 18:46:01 +0200

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-let boardState = ['', '', '', '', '', '', '', '', ''];
-let turn = 'X';
-let winner = null;
 const board = document.querySelector('.board');
+const cells = document.querySelectorAll('.cell');
 const message = document.querySelector('.message');
-const restartButton = document.querySelector('.restart');
+const restartButton = document.querySelector('.restart-button');
 
-function renderBoard() {
-    board.innerHTML = '';
-    boardState.forEach((cell, index) => {
-      const tile = document.createElement('div');
-      tile.classList.add('tile');
-      tile.textContent = cell;
-      tile.addEventListener('click', () => handleClick(index));
-      board.appendChild(tile);
-    });
-  }
-  
+let currentPlayer = 'X';
+let gameIsOver = false;
 
-function handleClick(index) {
-  if (winner !== null || boardState[index] !== '') {
+function handleCellClick(event) {
+  const cell = event.target;
+
+  if (cell.textContent !== '' || gameIsOver) {
     return;
   }
-  
-  boardState[index] = turn;
-  renderBoard();
-  checkWin();
+
+  cell.textContent = currentPlayer;
+
+  if (currentPlayer === 'X') {
+    cell.style.color = 'red';
+  } else {
+    cell.style.color = 'green';
+  }
+
+  if (checkForWinner()) {
+    message.textContent = `${currentPlayer} wins!`;
+    gameIsOver = true;
+  } else if (checkForTie()) {
+    message.textContent = `Tie game!`;
+    gameIsOver = true;
+  } else {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    message.textContent = `${currentPlayer}'s turn`;
+  }
 }
 
-function checkWin() {
-  const winningCombos = [
+
+function checkForWinner() {
+  const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -36,58 +43,36 @@ function checkWin() {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
-  
-  for (let i = 0; i < winningCombos.length; i++) {
-    const [a, b, c] = winningCombos[i];
-    if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c]) {
-      winner = turn;
-      message.textContent = `${turn} has won!`;
-      endGame(`${turn} has won!`);
-      return;
-    }
-  }
 
-  if (!boardState.includes('')) {
-    message.textContent = "It's a tie!";
-    endGame("It's a tie!");
-  } else {
-    turn = turn === 'X' ? 'O' : 'X';
-    message.textContent = `It's ${turn}'s turn.`;
-  }
+  return winningCombinations.some(combination => {
+    const [a, b, c] = combination;
+    return (
+      cells[a].textContent &&
+      cells[a].textContent === cells[b].textContent &&
+      cells[a].textContent === cells[c].textContent
+    );
+  });
 }
 
-function animateBalloons() {
-  const colors = ['#FFD700', '#FF69B4', '#00FFFF', '#FFA500', '#7B68EE', '#8FBC8F'];
-  const container = document.querySelector('.container');
-
-  for (let i = 0; i < 30; i++) {
-    const balloon = document.createElement('div');
-    balloon.classList.add('balloon');
-    balloon.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-    balloon.style.animationDelay = `${i / 10}s`;
-    container.appendChild(balloon);
-  }
+function checkForTie() {
+  return Array.from(cells).every(cell => cell.textContent !== '');
 }
 
-function endGame(result) {
-  board.removeEventListener('click', handleClick);
-  restartButton.style.display = 'block';
-  animateBalloons();
+function handleRestartClick() {
+  currentPlayer = 'X';
+  gameIsOver = false;
+
+  cells.forEach(cell => {
+    cell.textContent = '';
+  });
+
+  message.textContent = `${currentPlayer}'s turn`;
 }
 
-function restart() {
-  boardState = ['', '', '', '', '', '', '', '', ''];
-  turn = 'X';
-  winner = null;
-  message.textContent = `It's ${turn}'s turn.`;
-  renderBoard();
-  restartButton.style.display = 'none';
-  board.addEventListener('click', handleClick);
-}
+cells.forEach(cell => {
+  cell.addEventListener('click', handleCellClick);
+});
 
-renderBoard();
-message.textContent = `It's ${turn}'s turn.`;
-board.addEventListener('click', handleClick);
-restartButton.addEventListener('click', restart);
+restartButton.addEventListener('click', handleRestartClick);
